60068fcb7242b5f37a88e62175130c33ffca2354
Tumelo Mogono <tumelomogono@gmail.com>
2024-05-30 22:45:16 +0200

    deleted the meme data file and fetched an api for the meme data using sideEffect. Then adjusted my getMemeImage function to get rid of the variable memesArray and instead utilize the array I get from the API to get the URL's.

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import memesData from "../memesData.js"
 
 export default function Meme() {
-    // const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg")
     const [meme, setMeme ] = React.useState({
         topText: "",
         bottomText: "",
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
-    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    const [allMemes, setAllMemes] = React.useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemes(data.data.memes))
+    }, [])
 
     const getMemeImage = () => {
-        const memesArray = allMemeImages.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemes.length)
+        const url = allMemes[randomNumber].url
         setMeme(prevMeme => ({
             ...prevMeme,
             randomImage: url
         }))
     }
 
-    function handleChange(event) {
+    const handleChange = (event) => {
         const {name, value} = event.target
         setMeme(prevMeme => ({
             ...prevMeme,
@@ -47,7 +51,10 @@ export default function Meme() {
                     value={meme.bottomText}
                     onChange={handleChange}
                />
-               <button className="form--button" onClick={getMemeImage}>
+               <button 
+                    className="form--button" 
+                    onClick={getMemeImage}
+               >
                     Get a new meme image 🖼
                 </button> 
             </div>
